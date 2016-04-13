1e5c4275316e0401930fed845ea540371b1bebc1
Audun Meek Olsen <audunmeekolsen@gmail.com>
2016-04-13 16:46:24 +0200

    Lots of changes here bros
    First attempt at setting up angular,  adding additional jade markus,
    sass styles, and some js files

---
+++ js/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var app = angular.module('myApp', [
+	'ngRoute'
+]);
+
+var page1 = 'hjem';
+var page2 = 'yolo';
+var page3 = 'galleri';
+var page4 = 'om-oss';
+var page5 = 'kontakt';
+
+$('#page-1-link').text(page1);
+$('#page-2-link').text(page2);
+$('#page-3-link').text(page3);
+$('#page-4-link').text(page4);
+$('#page-5-link').text(page5);
+
+$("#page-1-link").attr("href", '#' + page1);
+$("#page-2-link").attr("href", '#' + page2);
+$("#page-3-link").attr("href", '#' + page3);
+$("#page-4-link").attr("href", '#' + page4);
+$("#page-5-link").attr("href", '#' + page5);
+/*
+$('page-one-desktop__link').textContent 	= pageOne;
+$('page-two-desktop__link').textContent 	= pageTwo;
+$('page-three-desktop__link').textContent = pageThree;
+$('page-four-desktop__link').textContent 	= pageFour;
+$('page-five-desktop__link').textContent 	= pageFive;
+
+$("page-one-desktop__link").setAttribute("href", '#' + pageOne);
+$("page-two-desktop__link").setAttribute("href", '#' + pageTwo);
+$("page-three-desktop__link").setAttribute("href", '#' + pageThree);
+$("page-four-desktop__link").setAttribute("href", '#' + pageFour);
+$("page-five-desktop__link").setAttribute("href", '#' + pageFive);
+*/
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+	.when('/' + page1,{
+	templateUrl: 'partials/default.html'
+	})
+	.when('/' + page2,{
+	templateUrl: 'partials/meny.html'
+	})
+	.when('/' + page3,{
+	templateUrl: 'partials/galleri.html'
+	})
+	.when('/' + page4,{
+	templateUrl: 'partials/om-oss.html'
+	})
+	.when('/' + page5,{
+	templateUrl: 'partials/kontakt.html'
+	})
+	.otherwise({
+	templateUrl: 'partials/default.html'
+	});
+}]);
