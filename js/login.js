4f0f2092967c1a3a28ecfe2584591749fe5d9a65
Karmany <haakonhorvei@hotmail.com>
2016-04-13 17:16:47 +0200

    Login

--- js/login.js
+++ js/login.js
@@ -1,27 +1,24 @@
-var alert, console;
-
-var a = document.forms.logmein.email.value,
-    b = document.forms.logmein.password.value;
-
-var studentEmail = "student@aurora.com";
-var studentPassword = "studentpwd";
-var teacherEmail = "teacher@aurora.com";
-var teacherPassword = "teacherpwd";
-var adminEmail = "admin@aurora.com";
-var adminPassword = "adminpwd";
-
 function logIn() {
     "use strict";
+
+    var a = document.forms.logmein.email.value;
+    var b = document.forms.logmein.password.value;
+    var studentEmail = "student@aurora.com";
+    var studentPassword = "studentpwd";
+    var teacherEmail = "teacher@aurora.com";
+    var teacherPassword = "teacherpwd";
+    var adminEmail = "admin@aurora.com";
+    var adminPassword = "adminpwd";
+
     if (a === studentEmail && b === studentPassword) {
-        window.location = "http://www.google.com";
+        window.location = "http://www.google.com/";
     }
     if (a === teacherEmail && b === teacherPassword) {
-        window.location = "http://www.google.com";
+        window.location = "http://www.google.com/";
     }
     if (a === adminEmail && b === adminPassword) {
-        window.location = "http://www.google.com";
+        window.location = "http://www.google.com/";
     } else {
         alert("Wrong login!");
     }
-
 }
