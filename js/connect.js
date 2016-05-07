d84d42c35d6d3ac37e35aabae850b44bbf70724b
Håkon Horvei <haakonhorvei@hotmail.com>
2016-05-08 00:05:03 +0200

    Video bytter slide

--- js/connect.js
+++ js/connect.js
@@ -1,15 +1,52 @@
 
+//This is the video we want to monitor
+//var vid = document.getElementById('lectureVideo');
 
 //How long the video is in seconds. Eg. "10.026667"
-var duration = document.getElementById('lectureVideo').duration;
+//var duration = document.getElementById('lectureVideo').duration;
 
 //How far the video have played in seconds. Eg. "5.426438"
-var currentTime = document.getElementById('lectureVideo').currentTime;
+//var currentTime = document.getElementById('lectureVideo').currentTime;
+
+
+var vid = document.getElementById('lectureVideo');
+var timeStamp = 0;
+
+vid.ontimeupdate = function() {getStarted()};
+
+function getStarted() {
+   pageTime();
+   changeImg();
+}
+
+function pageTime() {
+    document.getElementById("demo").innerHTML = vid.currentTime;
+    timeStamp = vid.currentTime;
+    console.log("Page time is updated to " + vid.currentTime);
+};
+
+
+function changeImg() {
+   if (vid.currentTime > 0  && vid.currentTime < 5 && document.getElementById('slides').src != slides[0]) {
+      document.getElementById('slides').src = slides[0];
+      console.log("Slide 1 loaded on page");
+   } else if (vid.currentTime > 5 && vid.currentTime < 10 && document.getElementById('slides').src != slides[1]) {
+      document.getElementById('slides').src = slides[1];
+      console.log("Slide 2 loaded on page");
+   }
+};
+
+
+
+
+
+
+
 
 //Links to the different slides that we will put in the page
 var slides = [
-   "../img/slides/slide1.png",
-   "../img/slides/slide2.png",
+   "../img/slides/joker.jpg",
+   "../img/slides/obama.jpg",
    "../img/slides/slide3.png",
    "../img/slides/slide4.png",
    "../img/slides/slide5.png",
